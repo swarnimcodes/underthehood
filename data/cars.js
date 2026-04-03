b35171c5f88c32a718a2e8526bca3ba856282e3c
Swarnim <swarnim@coditation.com>
2026-04-03 16:30:35 +0530

    Split data into separate files

---
+++ data/cars.js
@@ -0,0 +1,164 @@
+const cars = [
+    {
+        id: "tata-harrier",
+        name: "Tata Harrier",
+        brand: "Tata",
+        engines: ["fiat-multijet-II"]
+    },
+    {
+        id: "tata-safari",
+        name: "Tata Safari",
+        brand: "Tata",
+        engines: ["fiat-multijet-II"]
+    },
+    {
+        id: "mg-hector",
+        name: "MG Hector",
+        brand: "MG",
+        engines: ["fiat-multijet-II"]
+    },
+    {
+        id: "jeep-meridian",
+        name: "Jeep Meridian",
+        brand: "Jeep",
+        engines: ["fiat-multijet-II"]
+    },
+    {
+        id: "jeep-compass",
+        name: "Jeep Compass",
+        brand: "Jeep",
+        engines: ["fiat-multijet-II"]
+    },
+    {
+        id: "toyota-hyryder",
+        name: "Toyota Hyryder",
+        brand: "Toyota",
+        engines: ["suzuki-k15C", "toyota-m15D-FXE"]
+    },
+    {
+        id: "suzuki-grand-vitara",
+        name: "Suzuki Grand Vitara",
+        brand: "Suzuki",
+        engines: ["suzuki-k15C", "toyota-m15D-FXE"]
+    },
+    {
+        id: "kia-seltos",
+        name: "Kia Seltos",
+        brand: "Kia",
+        engines: ["hyundai-g4fl", "hyundai-g4lh", "hyundai-d4fb"]
+    },
+    {
+        id: "kia-sonet",
+        name: "Kia Sonet",
+        brand: "Kia",
+        engines: ["hyundai-g4fl", "hyundai-g4lh", "hyundai-d4fb"]
+    },
+    {
+        id: "hyundai-creta",
+        name: "Hyundai Creta",
+        brand: "Hyundai",
+        engines: ["hyundai-g4fl", "hyundai-g4lh", "hyundai-d4fb"]
+    },
+    {
+        id: "honda-city",
+        name: "Honda City",
+        brand: "Honda",
+        engines: ["honda-l15b", "honda-l15b-hev"]
+    },
+    {
+        id: "honda-elevate",
+        name: "Honda Elevate",
+        brand: "Honda",
+        engines: ["honda-l15b"]
+    },
+    {
+        id: "toyota-fortuner",
+        name: "Toyota Fortuner",
+        brand: "Toyota",
+        engines: ["toyota-1gd-ftv"]
+    },
+    {
+        id: "toyota-hilux",
+        name: "Toyota Hilux",
+        brand: "Toyota",
+        engines: ["toyota-1gd-ftv"]
+    },
+    {
+        id: "toyota-innova-hycross",
+        name: "Toyota Innova Hycross",
+        brand: "Toyota",
+        engines: ["toyota-m20a-fks", "toyota-m20a-fxs"]
+    },
+    {
+        id: "toyota-corolla",
+        name: "Toyota Corolla",
+        brand: "Toyota",
+        engines: ["toyota-m20a-fxs", "toyota-a25a-fxs"]
+    },
+    {
+        id: "toyota-camry",
+        name: "Toyota Camry",
+        brand: "Toyota",
+        engines: ["toyota-a25a-fxs"]
+    },
+    {
+        id: "suzuki-jimny",
+        name: "Suzuki Jimny",
+        brand: "Suzuki",
+        engines: ["suzuki-k15b"]
+    },
+    {
+        id: "suzuki-ciaz",
+        name: "Suzuki Ciaz",
+        brand: "Suzuki",
+        engines: ["suzuki-k15b"]
+    },
+    {
+        id: "vw-virtus",
+        name: "VW Virtus",
+        brand: "Volkswagen",
+        engines: ["vw-ea211-tsi", "vw-ea211-evo2"]
+    },
+    {
+        id: "skoda-slavia",
+        name: "Skoda Slavia",
+        brand: "Skoda",
+        engines: ["vw-ea211-tsi", "vw-ea211-evo2"]
+    },
+    {
+        id: "vw-taigun",
+        name: "VW Taigun",
+        brand: "Volkswagen",
+        engines: ["vw-ea211-tsi", "vw-ea211-evo2"]
+    },
+    {
+        id: "skoda-kushaq",
+        name: "Skoda Kushaq",
+        brand: "Skoda",
+        engines: ["vw-ea211-tsi", "vw-ea211-evo2"]
+    },
+    {
+        id: "mahindra-thar-roxx",
+        name: "Mahindra Thar Roxx",
+        brand: "Mahindra",
+        engines: ["mahindra-mhawk", "mahindra-mstallion"]
+    },
+    {
+        id: "mahindra-xuv700",
+        name: "Mahindra XUV700",
+        brand: "Mahindra",
+        engines: ["mahindra-mhawk", "mahindra-mstallion"]
+    },
+    {
+        id: "mahindra-xuv7xo",
+        name: "Mahindra XUV 7XO",
+        brand: "Mahindra",
+        engines: ["mahindra-mhawk", "mahindra-mstallion"]
+    },
+    {
+        id: "honda-civic",
+        name: "Honda Civic",
+        brand: "Honda",
+        engines: ["honda-l15c7"]
+    }
+];
